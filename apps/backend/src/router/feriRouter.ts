e950bd0133722b148ca46950216edc2ccd7b30f2
Vibhor Phalke <vibgitcode27@gmail.com>
2025-04-18 15:44:59 +0530

    add new routes

--- apps/backend/src/router/feriRouter.ts
+++ apps/backend/src/router/feriRouter.ts
@@ -1,21 +1,135 @@
 import { Router } from "express";
 import { authMiddleware } from "../middlewares";
+import { FeriCreateSchema, EditFeriCreateSchema } from "../types";
+import { prisma } from "./userRouter";
 
 const router = Router();
 
+//create a new feri
 router.post("/", authMiddleware, (req, res) => {
-  console.log("Received create request:", req.body);
+  const body = req.body;
+  const parsedBody = FeriCreateSchema.safeParse(body);
+  if (!parsedBody.success) {
+    console.error("Validation error:", parsedBody.error);
+    res.status(400).json({ error: "Invalid request data" });
+    return;
+  }
+
+  prisma.feri.create({
+    data: {
+      name: parsedBody.data.name,
+      status: parsedBody.data.status,
+      user: {
+        connect: { id: req.id },
+      },
+    },
+  });
+
   res.json({ message: "Feri created" });
 });
 
-router.get("/", authMiddleware, (req, res) => {
-  console.log("Received get all feri request:", req.body);
-  res.json({ message: "Feri sent" });
+//edit a feri
+router.put("/:feriId", authMiddleware, (req, res) => {
+  console.log("Received update request:", req.body);
+
+  const body = req.body;
+  const parsedBody = EditFeriCreateSchema.safeParse(body);
+  if (!parsedBody.success) {
+    console.error("Validation error:", parsedBody.error);
+    res.status(400).json({ error: "Invalid request data" });
+    return;
+  }
+
+  prisma.feri.update({
+    where: {
+      id: req.params.feriId,
+    },
+    data: {
+      name: parsedBody.data.name,
+      status: parsedBody.data.status,
+      trigger: {
+        create: {
+          availableTriggerId: parsedBody.data.trigger.availableTriggerId,
+        },
+      },
+      action: {
+        create: parsedBody.data.actions.map((action, index) => ({
+          availableActionId: action.availableActionId,
+          sortingOrder: index,
+        })),
+      },
+    },
+  });
+  res.json({ message: "Feri updated" });
 });
 
-router.get("/:feriId", authMiddleware, (req, res) => {
-  console.log("Received create request:", req.body);
-  res.json({ message: "Feri created" });
+// Get all feris
+router.get("/", authMiddleware, async (req, res) => {
+  try {
+    const response = await prisma.feri.findMany({
+      where: {
+        userId: req.id,
+      },
+      include: {
+        trigger: {
+          include: {
+            type: true,
+          },
+        },
+        action: {
+          include: {
+            type: true,
+          },
+        },
+        FeriRuns: {
+          include: {
+            feriOutbox: true,
+          },
+        },
+      },
+    });
+    res.status(200).json(response);
+  } catch (error) {
+    console.error("Error fetching feris:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Get a single feri
+router.get("/:feriId", authMiddleware, async (req, res) => {
+  try {
+    const response = await prisma.feri.findUnique({
+      where: {
+        userId: req.id,
+        id: req.params.feriId,
+      },
+      include: {
+        trigger: {
+          include: {
+            type: true,
+          },
+        },
+        action: {
+          include: {
+            type: true,
+          },
+        },
+        FeriRuns: {
+          include: {
+            feriOutbox: true,
+          },
+        },
+      },
+    });
+    if (response === null) {
+      res.status(404).json({ error: "Feri not found" });
+      return;
+    }
+    res.status(200).json(response);
+  } catch (error) {
+    console.error("Error fetching feris:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 export const feriRouter = router;
--- apps/backend/src/types/index.ts
+++ apps/backend/src/types/index.ts
@@ -10,3 +10,31 @@ export const LoginSchema = z.object({
   email: z.string().email(),
   password: z.string().min(6),
 });
+
+export const FeriCreateSchema = z.object({
+  name: z.string().min(1),
+  status: z.boolean(),
+  availableTrigerId: z.string(),
+  triggerMetadata: z.any().optional(),
+  actions: z.array(
+    z.object({
+      availableActionId: z.string(),
+      actionMetadata: z.any().optional(),
+    })
+  ),
+});
+
+export const EditFeriCreateSchema = z.object({
+  name: z.string().min(1),
+  status: z.boolean(),
+  triggerId: z.string(),
+  actions: z.array(
+    z.object({
+      availableActionId: z.string(),
+      actionMetadata: z.any().optional(),
+    })
+  ),
+  trigger: z.object({
+    availableTriggerId: z.string(),
+  }),
+});
